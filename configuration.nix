8716a7c5110a5ebb2abec7c408e47551b2ff9757
Ezequiel Buck <119618678+Ezequiel294@users.noreply.github.com>
2025-07-28 15:58:11 -0600

    first attempt to add modularity

--- configuration.nix
+++ configuration.nix
@@ -6,8 +6,9 @@
 
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [
       ./hardware-configuration.nix
+      ./packages.nix
     ];
 
   # Bootloader.
@@ -147,76 +148,6 @@
     "x-scheme-handler/http" = "firefox-devedition.desktop";
     "x-scheme-handler/https" = "firefox-devedition.desktop";
   };
-
-  fonts.packages = with pkgs; [
-     cantarell-fonts
-     nerd-fonts.jetbrains-mono
-  ];
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search packagename
-  environment.systemPackages = with pkgs; [
-     neovim 
-     wget
-     git
-     git-credential-manager
-     ghostty
-     waybar
-     swaylock
-     swayidle
-     wofi
-     brightnessctl
-     wl-clipboard
-     grim
-     slurp
-     tmux
-     fish
-     firefox-devedition
-     proton-pass
-     protonvpn-gui
-     keepassxc
-     yazi
-     lxappearance-gtk2
-     kdePackages.qt6ct
-     libsForQt5.qt5ct
-     starship
-     zoxide
-     gtk4
-     gtk3
-     kdePackages.breeze
-     libsForQt5.breeze-qt5
-     libsForQt5.breeze-gtk
-     pwvucontrol
-     kdePackages.breeze-icons
-     autotiling
-     btop
-     fastfetch
-     eza
-     bat
-     freerdp
-     insync
-     kanshi
-     lazygit
-     lazydocker
-     docker
-     nodejs_24
-     kdePackages.okular
-     onlyoffice-desktopeditors
-     kdePackages.partitionmanager
-     qalculate-qt
-     qbittorrent
-     spotify
-     vscode-fhs
-     xdg-desktop-portal-wlr
-     zip
-     unzip
-     python314
-     gcc
-     lxqt.lxqt-policykit
-     networkmanagerapplet
-     chromium
-     dropbox
-  ];
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
---
+++ packages.nix
@@ -0,0 +1,73 @@
+{ config, pkgs, ... }:
+
+}
+  fonts.packages = with pkgs; [
+     cantarell-fonts
+     nerd-fonts.jetbrains-mono
+  ];
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search packagename
+  environment.systemPackages = with pkgs; [
+     neovim 
+     wget
+     git
+     git-credential-manager
+     ghostty
+     waybar
+     swaylock
+     swayidle
+     wofi
+     brightnessctl
+     wl-clipboard
+     grim
+     slurp
+     tmux
+     fish
+     firefox-devedition
+     proton-pass
+     protonvpn-gui
+     keepassxc
+     yazi
+     lxappearance-gtk2
+     kdePackages.qt6ct
+     libsForQt5.qt5ct
+     starship
+     zoxide
+     gtk4
+     gtk3
+     kdePackages.breeze
+     libsForQt5.breeze-qt5
+     libsForQt5.breeze-gtk
+     pwvucontrol
+     kdePackages.breeze-icons
+     autotiling
+     btop
+     fastfetch
+     eza
+     bat
+     freerdp
+     insync
+     kanshi
+     lazygit
+     lazydocker
+     docker
+     nodejs_24
+     kdePackages.okular
+     onlyoffice-desktopeditors
+     kdePackages.partitionmanager
+     qalculate-qt
+     qbittorrent
+     spotify
+     vscode-fhs
+     xdg-desktop-portal-wlr
+     zip
+     unzip
+     python314
+     gcc
+     lxqt.lxqt-policykit
+     networkmanagerapplet
+     chromium
+     dropbox
+  ];
+}
